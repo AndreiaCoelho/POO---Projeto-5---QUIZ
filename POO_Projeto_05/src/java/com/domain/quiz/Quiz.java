f65ace387bbafe6fd049773dc417aa1612811656
AndreiaCoelho <andreiacoelho_2011@hotmail.com>
2017-05-16 00:42:04 -0300

    Ajustes no JSP Teste e Classe Java Quiz

--- POO_Projeto_05/src/java/com/domain/quiz/Quiz.java
+++ POO_Projeto_05/src/java/com/domain/quiz/Quiz.java
@@ -15,40 +15,40 @@ public class Quiz {
         if(test == null){
             test = new ArrayList<>();
             Question q1 = new Question
-            ("1) Qual a família mais rica da série Game Of Thrones?", "Lannister", new String[]{"Lannister","Tyrell","Stark"});
+            ("Qual a família mais rica da série Game Of Thrones?", "Lannister", new String[]{"Lannister","Tyrell","Stark"});
             test.add(q1);
             Question q2 = new Question
-            ("2) Qual o símbolo da casa Stark?", "Um lobo", new String[]{"Um lobo","Um dragão","Um leão"});
+            ("Qual o símbolo da casa Stark?", "Um lobo", new String[]{"Um lobo","Um dragão","Um leão"});
             test.add(q2);
             Question q3 = new Question
-            ("3) Qual o lema da casa Lannister?", "Ouça me rugir", new String[]{"Ouça me rugir","Todo Lannister paga sua divída","O inverno está chegando"});
+            ("Qual o lema da casa Lannister?", "Ouça me rugir", new String[]{"Ouça me rugir","Todo Lannister paga sua divída","O inverno está chegando"});
             test.add(q3);
             Question q4 = new Question
-            ("4) Quantos julgamentos por combate o Tyrion teve?", "2", new String[]{"2","1","3"});
+            ("Quantos julgamentos por combate o Tyrion teve?", "2", new String[]{"2","1","3"});
             test.add(q4);
             
             Question q5 = new Question
-            ("5) Quem é o pai biológico de Jon Snow?", "Rhaegar Targaryen", new String[]{"Rhaegar Targaryen","Aerys Targaryen","Ned Stark"});
+            ("Quem é o pai biológico de Jon Snow?", "Rhaegar Targaryen", new String[]{"Rhaegar Targaryen","Aerys Targaryen","Ned Stark"});
             test.add(q5);
             
             Question q6 = new Question
-            ("6) Quem empurrou Bran Stark da torre?", "Jaime", new String[]{"Jaime","Cersei","Tyrion"});
+            ("Quem empurrou Bran Stark da torre?", "Jaime", new String[]{"Jaime","Cersei","Tyrion"});
             test.add(q6);
             
             Question q7 = new Question
-            ("7) Meu Sol e Estrelas / Lua da minha vida - De qual casal pertemce essas falas?", "Daenerys e Drogo", new String[]{"Daenerys e Drogo","Jon Snow e Igritte","Jofrfrey e Sansa"});
+            ("Meu Sol e Estrelas / Lua da minha vida - De qual casal pertence essas falas?", "Daenerys e Drogo", new String[]{"Daenerys e Drogo","Jon Snow e Igritte","Joffrey e Sansa"});
             test.add(q7);
             
             Question q8 = new Question
-            ("8) Qual personagem fala a frase: Você não sabe de nada?", "Igritte", new String[]{"Igritte","Cersei","Sansa"});
+            ("Qual personagem fala a frase: Você não sabe de nada?", "Igritte", new String[]{"Igritte","Cersei","Sansa"});
             test.add(q8);
             
             Question q9 = new Question
-            ("9) Qual a religião que a personagem Arya está seguindo?", "Deus de muitas faces", new String[]{"Deus de muitas faces","Deus vermelho","Deus afogado"});
+            ("Qual a religião que a personagem Arya está seguindo?", "Deus de muitas faces", new String[]{"Deus de muitas faces","Deus vermelho","Deus afogado"});
             test.add(q9);
             
             Question q10 = new Question
-            ("10) Quem foi o primeiro rei a morrer?", "Robert", new String[]{"Robert","Joffrey","Renly"});
+            ("Quem foi o primeiro rei a morrer?", "Robert", new String[]{"Robert","Joffrey","Renly"});
             test.add(q10);
                                 
         }
